97b7a2bd031e5918a3c735c5292ea274dff0cea7
Cornelius Marian <cdmarian@liberty.edu>
2026-05-04 11:05:44 -0400

    New configurations to achieve 88% accuracy

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ FAISS_DIMENSION = 1024  # mxbai-embed-large specs
 FAISS_NLIST = 100       # this is set for 10,000 chunks change if necessary
 FAISS_M = 32            # change based off embedding model specs
 FAISS_NBITS = 8         # for my precious RAM
-NPROBE = 10             # number of neigbors (voroni cells) to search 
-KWARGS = 4              # amount of relevant results
+NPROB = 20              # number of neigbors (voroni cells) to search 
+KWARGS = 8              # amount of relevant results
 
 CHUNK_SIZE = 1000       # based off use case and slm model
 CHUNK_OVERLAP = 200     # prevents cutting off info in chunks 
